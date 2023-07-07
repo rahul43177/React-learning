21218761076e16f0b3c0c09d819f1d983b7fbd8f
Rahul <rahulmishra4317@gmial.com>
2023-07-07 18:35:00 +0530

    create app profileCard props

---
+++ src/ProfileCard.js
@@ -0,0 +1,11 @@
+function ProfileCard({title , handle}) {
+    
+    return (
+        <div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+        </div>
+    )
+
+}
+export default ProfileCard
