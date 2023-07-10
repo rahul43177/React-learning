17aa147ef15695e2a5d1020d65a43f996f61b48c
Rahul <rahulmishra4317@gmial.com>
2023-07-10 12:52:14 +0530

    created first personal assistant application

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React - project 1 
+## Create a project which shows the details about cortana , siri and alexa with image 
+
--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import ProfileCard  from './ProfileCard'
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import SiriImage from "./images/siri.png";
+import CortanaImage from "./images/cortana.png";
 
+console.log(AlexaImage);
+console.log(SiriImage);
+console.log(CortanaImage);
 function App() {
-    return (
-        <div>
-            <div>Personal digital assistant</div>
-            <ProfileCard title=" Alexa" handle="@alexa99"/>
-            <ProfileCard title=" Cortana" handle="@cortana32"/>
-            <ProfileCard title=" Siri" handle="@siri01"/>
-            
+  return (
+    <div>
+        <section className="hero is-link">
+            <div className="hero-body">
+                <p className="title">
+                    Personal Assistance 
+                </p>
+            </div>
+        </section>
+
+
+        <div className="container">
+            <section className="section">
+                <div className="columns">
+                    <div className="column is-4">
+                        <ProfileCard
+                            title="Alexa"
+                            handle="@alexa"
+                            image={AlexaImage}
+                            description="Alexa was created by Amazon and it helps you buy things"
+                        />
+                    </div>
+                    <div className="column is-4">
+                        <ProfileCard
+                            title="Siri"
+                            handle="@siri"
+                            image={SiriImage}
+                            description = "Cortana was made by microsoft"
+                        />
+                    </div>
+                    <div className="column is-4">
+                        <ProfileCard
+                            title="Cortana"
+                            handle="@cortana"
+                            image={CortanaImage}
+                            description = "Siri is an apple product"
+                        />
+                    </div>
+                </div>
+            </section>
         </div>
-    )
+
+
+      </div>
+      
+  );
 }
 
-export default App
+export default App;
